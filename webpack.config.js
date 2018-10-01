d6a8af1c0da923fe60f8f38df912258c157f18dc
Justin <jeh5256@gmail.com>
2018-09-30 21:45:09 -0500

    Setup Webpack for prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,51 @@
 const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'main.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-   
-    devServer: {
-        contentBase:  path.join(__dirname, 'public'),
-        host: '127.0.0.1',
-        historyApiFallback: true
-    }
+module.exports = (env) => {
+    const isProd = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'main.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },{
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },{
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            // Ignore all locale files of moment.js
+            new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+            CSSExtract
+        ],
+        devtool: isProd ? 'source-map' : 'inline-source-map',
+    
+        devServer: {
+            contentBase:  path.join(__dirname, 'public'),
+            host: '127.0.0.1',
+            historyApiFallback: true
+        }
+    };
 };
