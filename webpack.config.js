274834827b323ece3f0d7f10c965845f7f037837
Justin <jeh5256@gmail.com>
2018-11-11 22:44:30 -0600

    fix spelling in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = (env) => {
     const isProd = env === 'production';
     const CSSExtract = new ExtractTextPlugin('styles.css');
     return {
-        entry: ['bable-polyfill', './src/app.js'],
+        entry: ['babel-polyfill', './src/app.js'],
         output: {
             path: path.join(__dirname, 'public', 'dist'),
             filename: 'main.js'
